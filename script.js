9a20788e812feb8816f67f9618f2396f826675f2
Jacob Fratti <JacobRFratti@gmail.com>
2024-03-08 16:24:34 -0500

    Added pseudo code to help organize my thoughts

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Get Special Characters
+// Get numbers
+// Get Lower case letters
+// Get uppercase letters
+
+// Prompt to determine password length
+// Prompt to confirm password options
+//    - Characters
+//    - Numbers
+//    - Lowercase letters
+//    - Uppercase letters
+
+// Generate password
+// Gather all available options from user selection
+// Randomize the pool of options and select amount from length requested
+
+// Turn to a string and send it to write password
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
